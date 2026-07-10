4a5754ac2b1663bb37936f3c00ca4a070614c34f
doksanoglusamet61 <doksanoglusamet61@gmail.com>
2026-07-10 22:45:31 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,7 +1,14 @@
-smsmsmms
+-- Basit bir MTA:SA client-side Lua örneği
 
-sFJJFLJSFL
+addEventHandler("onClientResourceStart", resourceRoot, function()
+    outputChatBox("MTA Lua script çalıştı!", 255, 255, 0, true)
+end)
 
+bindKey("F1", "down", function()
+    outputChatBox("F1 tuşuna basıldı!", 0, 255, 0, true)
+end)
 
-
-hhhh
+addEventHandler("onClientRender", root, function()
+    local screenW, screenH = guiGetScreenSize()
+    dxDrawText("MTA Lua örneği", 20, 20, screenW, screenH, tocolor(255, 255, 255, 255), 1.2, "default-bold")
+end)
